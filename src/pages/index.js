c1c6cb8b0f5e9825a04f062aa346089cdf7d0387
OlStoppa <olthespud@hotmail.com>
2019-11-11 17:01:42 +0800

    altered styles at 1024px

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Carousel from "../components/carousel"
 import Image from "../components/image"
@@ -15,7 +16,7 @@ const IndexPage = () => {
           <div className="carousel-footer">
             <h1>SAVE UP TO</h1>
             <h1 className="sale-h1">50%</h1>
-            <div className="shop-now">SHOP</div>
+            <Link to="/collection/pullover"><div className="shop-now">SHOP</div></Link>
           </div>
         </div>
         <div className="home-top__right">
@@ -23,14 +24,14 @@ const IndexPage = () => {
             <Image filename="festival.jpg" />
             <div className="box-footer">
               <h4>HUGE FESTIVAL SEASON DISCOUNTS</h4>
-              <div className="shop-now">SHOP</div>
+              <Link to="/collection/pullover"><div className="shop-now">SHOP</div></Link>
             </div>
           </div>
           <div className="home-top__right--box bottom">
             <Image filename="anime-front.PNG" />
             <div className="box-footer">
               <h4>THE RAWEST NEW JAPANESE DESIGNS</h4>
-              <div className="shop-now">SHOP</div>
+              <Link to="/collection/pullover"><div className="shop-now">SHOP</div></Link>
             </div>
           </div>
         </div>
